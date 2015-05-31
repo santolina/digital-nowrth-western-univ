1ad2990ba7262e963d23d0f5f49ae7f84d26a5b9
santolina <tsukuba.takeshi@gmail.com>
2015-05-31 23:05:27 +0900

    Create week8q7.m

---
+++ homework/week8q7.m
@@ -0,0 +1,45 @@
+% This is a sample code to solve week8 quiz 7
+% Here is a link to quizz: https://class.coursera.org/digital-002/quiz/feedback?submission_id=31776
+% Here is a link to iamge "Cameraman256.bmp": https://d396qusza40orc.cloudfront.net/digital/images/week8_quizzes/Cameraman256.bmp
+
+MAXI = double(255);
+
+% read input image and show it
+I1 = (imread('Cameraman256.bmp'));
+
+figure, imshow(I1);
+
+% calc histogram
+Hist = zeros(256);
+
+%vmse = double(0);
+SIZE=256;
+for j = 1: SIZE
+    for i = 1: SIZE
+        idx = I1(i,j);
+        Hist(idx+1) = Hist(idx+1) + 1;
+    end
+end
+
+% normalize histogram, i.e prob
+P = zeros(256);
+for j = 1: SIZE
+    P(j) = double(Hist(j) / (MAXI*MAXI));
+end
+
+figure, plot(P);
+
+% calc entropy
+I = zeros(256);
+for i = 1 : SIZE
+    if P(i) == 0
+        I(i) = 0;
+    end
+    I(i) = - double(P(i)) * double(log2(P(i)));
+end
+figure, plot(I);
+
+H = double(0);
+for i = 1 : SIZE-1
+    H = H + double(I(i));
+end
